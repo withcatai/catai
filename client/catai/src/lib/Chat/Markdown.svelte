437e77d91fe58b2cf6cb21c65a9d8d5c8b14ffed
Ido S <ido.pluto@gmail.com>
2023-05-19 19:42:55 +0300

    fix(markdown): line height + copy icon

--- client/catai/src/lib/Chat/Markdown.svelte
+++ client/catai/src/lib/Chat/Markdown.svelte
@@ -28,7 +28,9 @@
             const copyText = str.replaceAll('"', '&quot;').replaceAll('\'', '&lt;').replaceAll('\n', '\\n');
             return `<div style="position: relative">${
                 ''
-            }<button class="copy-clipboard text-white bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-xs p-2 shadow-green-500" onclick="copyToClipboard('${copyText}')">Copy</button>${
+                }<button class="copy-clipboard text-white bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-xs p-2 shadow-green-500" onclick="copyToClipboard('${copyText}')">${''
+                    }<svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" aria-label="clipboard" fill="none" viewBox="0 0 24 24" stroke-width="1.5"><path d="M15.6657 3.88789C15.3991 2.94272 14.5305 2.25 13.5 2.25H10.5C9.46954 2.25 8.60087 2.94272 8.33426 3.88789M15.6657 3.88789C15.7206 4.0825 15.75 4.28782 15.75 4.5V4.5C15.75 4.91421 15.4142 5.25 15 5.25H9C8.58579 5.25 8.25 4.91421 8.25 4.5V4.5C8.25 4.28782 8.27937 4.0825 8.33426 3.88789M15.6657 3.88789C16.3119 3.93668 16.9545 3.99828 17.5933 4.07241C18.6939 4.20014 19.5 5.149 19.5 6.25699V19.5C19.5 20.7426 18.4926 21.75 17.25 21.75H6.75C5.50736 21.75 4.5 20.7426 4.5 19.5V6.25699C4.5 5.149 5.30608 4.20014 6.40668 4.07241C7.04547 3.99828 7.68808 3.93668 8.33426 3.88789" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>${''
+                }</button>${
                 htmlContent
             }</div>`;
         }
@@ -64,6 +66,10 @@
 
     ol, ul, menu {
       list-style: auto !important;
+    }
+
+    li {
+      line-height: .5rem;
     }
 
     .copy-clipboard {
