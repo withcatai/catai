e611dd0e54d1ae6585a317b56723a1ead9a77198
Ido S <ido.pluto@gmail.com>
2023-05-19 19:24:12 +0300

    feat(ui): copy code

--- client/catai/src/lib/Chat/Markdown.svelte
+++ client/catai/src/lib/Chat/Markdown.svelte
@@ -3,6 +3,12 @@
     import '../../assets/css/highlight.css';
     import '../../assets/css/markdown.css';
     import HighlightJS from 'highlight.js';
+    import {useTimeout} from '@svelte-use/core';
+    import {Toast} from 'flowbite-svelte';
+    import {Clipboard} from 'svelte-heros-v2';
+    import {blur} from 'svelte/transition';
+
+    const {start, ready} = useTimeout(3000, {controls: true, immediate: false});
 
     const md = markdownIt({
         html: true,
@@ -10,16 +16,28 @@
         typographer: true,
         breaks: false,
         highlight: function (str, lang) {
+            let htmlContent = str;
             try {
-                if(!HighlightJS.getLanguage(lang)){
-                    return HighlightJS.highlightAuto(str).value;
+                if (!HighlightJS.getLanguage(lang)) {
+                    htmlContent = HighlightJS.highlightAuto(str).value;
+                } else {
+                    htmlContent = HighlightJS.highlight(str, {language: lang}).value;
                 }
-                return HighlightJS.highlight(str, {language: lang}).value;
-            } catch {
-                return str;
-            }
+            } catch {}
+
+            const copyText = str.replaceAll('"', '&quot;').replaceAll('\'', '&lt;').replaceAll('\n', '\\n');
+            return `<div style="position: relative">${
+                ''
+            }<button class="copy-clipboard text-white bg-green-700 hover:bg-green-800 dark:bg-green-600 dark:hover:bg-green-700 focus:ring-green-300 dark:focus:ring-green-800 rounded-lg text-xs p-2 shadow-green-500" onclick="copyToClipboard('${copyText}')">Copy</button>${
+                htmlContent
+            }</div>`;
         }
     });
+
+    window.copyToClipboard = function copyToClipboard(text: string) {
+        navigator.clipboard.writeText(text);
+        start();
+    }
 
     export let value = '';
     $: markedCode = md.render(value);
@@ -29,15 +47,28 @@
     {@html markedCode}
 </div>
 
-<style global>
-    .markdown-body {
-        background-color: transparent !important;
-        color: inherit !important;
-        font-size: inherit!important;
-        white-space: pre-wrap;
+
+{#if !$ready}
+    <Toast transition={blur} params={{x: 200}} color="green" class="mb-2" position="top-right">
+        <Clipboard slot="icon"/>
+        Text copied to clipboard
+    </Toast>
+{/if}
+
+<style lang="scss" global>
+  .markdown-body {
+    background-color: transparent !important;
+    color: inherit !important;
+    font-size: inherit !important;
+    white-space: pre-wrap;
+
+    ol, ul, menu {
+      list-style: auto !important;
     }
 
-    .markdown-body ol, ul, menu {
-        list-style: auto!important;
+    .copy-clipboard {
+      position: absolute;
+      right: 0;
     }
+  }
 </style>
