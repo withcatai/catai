da8bbbb6574fb847d757c884182e5e0e3fa4554f
Ido S <ido.pluto@gmail.com>
2023-06-09 18:11:29 +0300

    feat(migration): from v0

---
+++ server/scripts/migration/index.js
@@ -0,0 +1,22 @@
+import {$} from 'execa';
+import {fileURLToPath} from 'url';
+import {fs} from 'zx';
+import path from 'path';
+
+const __dirname = fileURLToPath(new URL('./', import.meta.url));
+const { version: newVersion } = await fs.readJSON(path.join(__dirname, '..', '..', 'package.json'));
+
+const {stdout: version} = await $`catai --version`;
+const fromVersion = Number(version.trim()[0]);
+const toVersion = Number(newVersion.trim()[0]);
+
+async function runMigrations(fromVersion, toVersion){
+    for(let i = fromVersion; i < toVersion; i++){
+        const migration = await import(`./versions/v${i}.js`);
+        await migration.default();
+    }
+}
+
+async function main() {
+    await runMigrations(fromVersion, toVersion);
+} main();
