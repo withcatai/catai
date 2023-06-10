f49a2da484b3910ce412be92604278da602ead71
Ido S <ido.pluto@gmail.com>
2023-06-10 21:51:19 +0300

    fix(migration): find old version

--- server/scripts/migration/index.js
+++ server/scripts/migration/index.js
@@ -6,7 +6,7 @@ import path from 'path';
 const __dirname = fileURLToPath(new URL('./', import.meta.url));
 const { version: newVersion } = await fs.readJSON(path.join(__dirname, '..', '..', 'package.json'));
 
-const {stdout: version} = await $`catai --version`;
+const {stdout: version} = await $`npm -g info catai version`;
 const fromVersion = Number(version.trim()[0]);
 const toVersion = Number(newVersion.trim()[0]);
 
