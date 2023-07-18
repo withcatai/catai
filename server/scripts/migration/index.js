924a01403f17a9b51612f459b3231b1ab48e350b
Ido S <ido.pluto@gmail.com>
2023-07-18 20:49:26 +0300

    fix(migration): first install

--- server/scripts/migration/index.js
+++ server/scripts/migration/index.js
@@ -7,7 +7,7 @@ const __dirname = fileURLToPath(new URL('./', import.meta.url));
 const { version: newVersion } = await fs.readJSON(path.join(__dirname, '..', '..', 'package.json'));
 
 const {stdout: version} = await $`npm -g info catai version`;
-const fromVersion = Number(version.trim()[0]);
+const fromVersion = Number(version.trim()[0] || '-');
 const toVersion = Number(newVersion.trim()[0]);
 
 async function runMigrations(fromVersion, toVersion){
