ccfc39be6e94c81875c6af65eab97039855e09fe
Ido S <ido.pluto@gmail.com>
2023-08-18 18:23:28 +0300

    fix: cli executable type

--- server/src/cli/cli.ts
+++ server/src/cli/cli.ts
@@ -1,15 +1,17 @@
-import {Command} from 'commander';
-import {installCommand} from './commands/install.js';
-import {listModelsCommand} from './commands/models.js';
-import {removeCommand} from './commands/remove.js';
-import {useCommand} from './commands/use.js';
-import {packageJSON} from '../storage/config.js';
-import {updateHook} from './hooks/update.js';
-import {updateCommand} from './commands/update.js';
-import {uninstallCommand} from './commands/uninstall.js';
-import {serveCommand} from './commands/serve.js';
-import {activeCommand} from './commands/active.js';
-import {postinstallCommand} from './commands/postinstall/postinstall.js';
+#!/usr/bin/env node
+
+import {Command} from "commander";
+import {installCommand} from "./commands/install.js";
+import {listModelsCommand} from "./commands/models.js";
+import {removeCommand} from "./commands/remove.js";
+import {useCommand} from "./commands/use.js";
+import {packageJSON} from "../storage/config.js";
+import {updateHook} from "./hooks/update.js";
+import {updateCommand} from "./commands/update.js";
+import {uninstallCommand} from "./commands/uninstall.js";
+import {serveCommand} from "./commands/serve.js";
+import {activeCommand} from "./commands/active.js";
+import {postinstallCommand} from "./commands/postinstall/postinstall.js";
 
 const program = new Command();
 program.version(packageJSON.version);
