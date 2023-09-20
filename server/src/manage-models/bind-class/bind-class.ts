b2ad9870aba573ce33f7351b5379eae3dd6f349f
Ido S <ido.pluto@gmail.com>
2023-09-20 08:07:42 +0300

    fix(create-chat): parallels initialization (#39)

--- server/src/manage-models/bind-class/bind-class.ts
+++ server/src/manage-models/bind-class/bind-class.ts
@@ -2,6 +2,7 @@ import BaseBindClass from './binds/base-bind-class.js';
 import AppDb from '../../storage/app-db.js';
 import NodeLlamaCppV1 from './binds/node-llama-cpp/node-llama-cpp-v1/node-llama-cpp-v1.js';
 import NodeLlamaCppV2 from './binds/node-llama-cpp/node-llama-cpp-v2/node-llama-cpp-v2.js';
+import AwaitLock from 'await-lock';
 
 export const ALL_BINDS = [NodeLlamaCppV1, NodeLlamaCppV2];
 const cachedBinds: { [key: string]: InstanceType<typeof BaseBindClass> } = {};
@@ -12,38 +13,45 @@ function getActiveModelDetails() {
     if (!modelDetails)
         throw new Error('No active model');
 
-    if(!modelDetails.settings.bind)
+    if (!modelDetails.settings.bind)
         throw new Error('No bind class');
 
     return modelDetails;
 }
 
-export function getCacheBindClass(){
+export function getCacheBindClass() {
     const modelDetails = getActiveModelDetails();
 
     const bind = modelDetails.settings.bind;
 
-    if(cachedBinds[bind])
+    if (cachedBinds[bind])
         return cachedBinds[bind];
 
     return null;
 }
-export default async function createChat(){
-    const modelDetails = getActiveModelDetails();
-    const cachedBindClass = getCacheBindClass();
 
-    if(cachedBindClass)
-        return cachedBindClass.createChat();
+const bindLock = new (typeof AwaitLock === 'function' ? AwaitLock : AwaitLock.default);
+export default async function createChat() {
+    try {
+        await bindLock.acquireAsync();
 
-    const bind = modelDetails.settings.bind;
+        const modelDetails = getActiveModelDetails();
+        const cachedBindClass = getCacheBindClass();
 
-    const bindClass = ALL_BINDS.find(x => x.shortName === bind);
-    if (!bindClass)
-        throw new Error(`Bind class ${bind} not found`);
+        if (cachedBindClass)
+            return cachedBindClass.createChat();
 
-    const bindClassInstance = cachedBinds[bind] ??= new bindClass(modelDetails);
-    await bindClassInstance.initialize();
+        const bind = modelDetails.settings.bind;
 
-    return bindClassInstance.createChat();
+        const bindClass = ALL_BINDS.find(x => x.shortName === bind);
+        if (!bindClass)
+            throw new Error(`Bind class ${bind} not found`);
+
+        const bindClassInstance = cachedBinds[bind] ??= new bindClass(modelDetails);
+        await bindClassInstance.initialize();
+        return bindClassInstance.createChat();
+    } finally {
+        bindLock.release();
+    }
 }
 
