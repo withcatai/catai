cf1694b523d3123d3f848b42236bd45dcf631980
ido <ido.pluto@w-on.uk>
2023-12-14 11:31:18 +0200

    fix: model settings

--- server/src/manage-models/bind-class/binds/node-llama-cpp/node-llama-cpp-v2/node-llama-cpp-v2.ts
+++ server/src/manage-models/bind-class/binds/node-llama-cpp/node-llama-cpp-v2/node-llama-cpp-v2.ts
@@ -1,13 +1,14 @@
-import type {LlamaChatSessionOptions, LlamaContextOptions, LlamaModel} from 'node-llama-cpp';
-import {LLamaChatPromptOptions} from 'node-llama-cpp';
+import type {LLamaChatPromptOptions, LlamaChatSessionOptions, LlamaContextOptions, LlamaModel, LlamaModelOptions} from 'node-llama-cpp';
 import NodeLlamaCppChat from './node-llama-cpp-chat.js';
 import BaseBindClass from '../../base-bind-class.js';
 
-const DEFAULT_CONTEXT_SIZE = 4096;
+type NodeLlamaCppOptions =
+    Omit<LlamaContextOptions, 'model'> &
+    Omit<LlamaModelOptions, 'modelPath'> &
+    Omit<LlamaChatSessionOptions, 'contextSequence'> &
+    LLamaChatPromptOptions;
 
-type NodeLlamaCppOptions = Omit<LlamaContextOptions, 'model'> & LLamaChatPromptOptions & {
-    wrapper?: string,
-};
+const DEFAULT_CONTEXT_SIZE = 4096;
 
 export default class NodeLlamaCppV2 extends BaseBindClass<NodeLlamaCppOptions> {
     public static override shortName = 'node-llama-cpp-v2';
@@ -15,21 +16,21 @@ export default class NodeLlamaCppV2 extends BaseBindClass<NodeLlamaCppOptions> {
     private _model?: LlamaModel;
     private _package?: typeof import('node-llama-cpp');
 
-    createChat(overrideSettings?: NodeLlamaCppOptions, sessionSettings?: LlamaChatSessionOptions): NodeLlamaCppChat {
+    createChat(overrideSettings?: NodeLlamaCppOptions): NodeLlamaCppChat {
         if (!this._model || !this._package)
             throw new Error('Model not initialized');
 
         const settings: NodeLlamaCppOptions = {...this.modelSettings.settings, ...overrideSettings};
+        settings.batchSize ??= settings.contextSize ??= DEFAULT_CONTEXT_SIZE;
 
         const context = new this._package.LlamaContext({
             model: this._model,
-            ...settings,
-            ...overrideSettings
+            ...settings
         });
 
         const session = new this._package.LlamaChatSession({
             contextSequence: context.getSequence(),
-            ...sessionSettings
+            ...settings
         });
 
         return new NodeLlamaCppChat(settings, session);
@@ -40,7 +41,8 @@ export default class NodeLlamaCppV2 extends BaseBindClass<NodeLlamaCppOptions> {
         this._package = others as any;
 
         this._model = new LlamaModel({
-            modelPath: this.modelSettings.downloadedFiles.model
+            modelPath: this.modelSettings.downloadedFiles.model,
+            ...this.modelSettings.settings
         });
     }
 }
